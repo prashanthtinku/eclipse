65d493ce6747cecba55dee0105ec71b00cceb880
Prashanth1 <aedulapuraprash380@students.itu.edu>
2016-08-28 09:50:06 -0700

    Renamed to JavaClass

--- JCProject/src/jcPackage/JCClass.java
+++ JCProject/src/jcPackage/JCClass.java
@@ -5,11 +5,11 @@ public class JCClass {
 	public static void main(String[] args) {
 		System.out.println("In Main");
 		JCClass jc=new JCClass();
-		jc.JCClass();
+		jc.JavaClass();
 
 	}
 	
-	public void JCClass(){
+	public void JavaClass(){
 		System.out.println("In the Function JC Class");
 	}
 
