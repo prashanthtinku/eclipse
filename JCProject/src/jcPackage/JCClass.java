e365615415f87003c0d09b4b7f2417761cd375a0
Prashanth1 <aedulapuraprash380@students.itu.edu>
2016-08-28 10:28:51 -0700

    Update in anotherBranch

--- JCProject/src/jcPackage/JCClass.java
+++ JCProject/src/jcPackage/JCClass.java
@@ -6,8 +6,8 @@ public class JCClass {
 		System.out.println("In Main changing the Message");
 		JCClass jc=new JCClass();
 		jc.JavaClass();
-		jc.secondFunction();	
-
+		jc.newOne();
+		jc.secondFunction();
 	}
 	
 	public void JavaClass(){
@@ -17,6 +17,13 @@ public class JCClass {
 	public void secondFunction(){
 		System.out.println("Second Entry");
 		System.out.println("In the second method");
+		for(int i=1;i<3;i++){
+			System.out.println("Hello "+i);
+		}
+	}
+	
+	public void newOne(){
+		System.out.println("In the New One");
 	}
 
 }
