88c3a791b715dc7376ee1140fd2be125a85bb2ab
Prashanth1 <aedulapuraprash380@students.itu.edu>
2016-08-28 09:48:07 -0700

    Added JCClass

---
+++ JCProject/src/jcPackage/JCClass.java
@@ -0,0 +1,16 @@
+package jcPackage;
+
+public class JCClass {
+
+	public static void main(String[] args) {
+		System.out.println("In Main");
+		JCClass jc=new JCClass();
+		jc.JCClass();
+
+	}
+	
+	public void JCClass(){
+		System.out.println("In the Function JC Class");
+	}
+
+}
